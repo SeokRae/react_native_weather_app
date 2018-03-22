a03ca6caadaf096289bbd767616c07181ea642ab
seokrae <kslbsh@gmail.com>
2018-03-22 16:36:38 +0900

    Commit #3-1. Building the Loading View

--- App.js
+++ App.js
@@ -1,52 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
+  // 데이터 api를 불러오게되면 이 값은 true가 된다.
+  // 로딩 중에는 false
+  state = {
+    isLoaded: false
+  };
   render() {
-    return <View style={styles.container}>
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-        <View style={styles.redView} />
-        <View style={styles.yellowView} />
-      </View>;
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null : (
+          <View style = {styles.loading}>
+            <Text style = {styles.loadingText}>
+              Getting the weather
+            </Text>
+          </View>
+        )}
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    flexDirection: "row",
-    flexWrap: 'wrap'
+    backgroundColor: "#fff"
   },
-  redView: {
-    height: 50,
-    width: 50,
-    backgroundColor: "red"
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end"
+    , paddingLeft: 25
   },
-  yellowView: {
-    height: 50,
-    width: 50,
-    backgroundColor: "yellow"
+  loadingText: {
+    fontSize: 40,
+    marginBottom: 100
   }
 });
