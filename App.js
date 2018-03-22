c52b71d6b93cc124dbd3795f4c5c33c27976f50b
seokrae <kslbsh@gmail.com>
2018-03-22 15:06:01 +0900

    Commit #2-2. React Native Layouts with Flexbox

--- App.js
+++ App.js
@@ -3,20 +3,50 @@ import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator />
-        <Text>Hello I'm Native Developer Now!~</Text>
-      </View>
-    );
+    return <View style={styles.container}>
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+        <View style={styles.redView} />
+        <View style={styles.yellowView} />
+      </View>;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    flexDirection: "row",
+    flexWrap: 'wrap'
   },
+  redView: {
+    height: 50,
+    width: 50,
+    backgroundColor: "red"
+  },
+  yellowView: {
+    height: 50,
+    width: 50,
+    backgroundColor: "yellow"
+  }
 });
